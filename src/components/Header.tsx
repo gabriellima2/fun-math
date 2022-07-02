ecc01d8fc58de34e1603a0732166b870bd5a8d70
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-02 17:13:42 -0300

    adiciona estilização para sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import { Sidebar } from "./Sidebar";
 
 export const Header = () => (
-	<header className="w-full flex justify-center p-3 px-4">
+	<header className="w-full flex justify-between items-center p-4 pt-6 px-8">
 		<Link href="/">
-			<a>
-				<Image src="/logo.svg" width={100} height={60} />
+			<a className="flex-center--row translate-y-1 z-50">
+				<Image src="/logo.svg" width={100} height={40} />
 			</a>
 		</Link>
 		<Sidebar />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { Dialog } from "@headlessui/react";
+import { BsFillGrid3X3GapFill, BsXLg } from "react-icons/bs";
 
 import { links } from "../contents/links";
 
 type NavLinkProps = typeof links[0];
 
 const NavLink = (props: NavLinkProps) => (
-	<li>
+	<li className="w-full">
 		<Link href={props.href}>
-			<>
-				<i>{React.createElement(props.icon, null)}</i>
-				<a>{props.name}</a>
-			</>
+			<a className="w-full bg-gradient-to-r from-accents-purple ... flex items-center gap-2 text-lg capitalize p-4 my-2 rounded-md">
+				<i className="text-2xl self-start">
+					{React.createElement(props.icon, null)}
+				</i>
+				{props.name}
+			</a>
 		</Link>
 	</li>
 );
@@ -21,21 +24,35 @@ export const Sidebar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	if (!isOpen) {
-		return <button onClick={() => setIsOpen(true)}>Abrir Sidebar</button>;
+		return (
+			<button
+				onClick={() => setIsOpen(true)}
+				className="justify-self-end text-2xl text-pink-50"
+			>
+				<BsFillGrid3X3GapFill />
+			</button>
+		);
 	}
 
 	return (
-		<Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-			<Dialog.Panel>
-				<Dialog.Title className="hidden">Navigation sidebar</Dialog.Title>
-				<nav>
+		<Dialog open={isOpen} onClose={() => setIsOpen(false)} className="z-50">
+			<div className="fixed inset-0 bg-black-800/60" aria-hidden="true" />
+
+			<Dialog.Panel className="w-full h-1/2 flex-center--col absolute right-0 top-0 z-45 bg-black-700">
+				<Dialog.Title className="hidden">Barra de navegação</Dialog.Title>
+				<button
+					onClick={() => setIsOpen(false)}
+					className="absolute top-8 right-8 text-2xl text-pink-50"
+				>
+					<BsXLg />
+				</button>
+				<nav className="w-full p-4">
 					<ul>
 						{links.map((link) => (
 							<NavLink {...link} key={link.id} />
 						))}
 					</ul>
 				</nav>
-				<button onClick={() => setIsOpen(false)}>Fechar Sidebar</button>
 			</Dialog.Panel>
 		</Dialog>
 	);
