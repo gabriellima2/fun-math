f0a52ee8d7a3b053aecca1c6971acf60e0b27f07
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-02 18:36:11 -0300

    [feat]: adiciona responsividade para sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ type NavLinkProps = typeof links[0];
 const NavLink = (props: NavLinkProps) => (
 	<li className="w-full">
 		<Link href={props.href}>
-			<a className="w-full bg-gradient-to-r from-accents-purple ... flex items-center gap-2 text-lg capitalize p-4 my-2 rounded-md">
+			<a className="w-full bg-gradient-to-r from-accents-purple ... flex items-center gap-2 text-lg capitalize p-4 my-2 rounded-md transition-hover hover:brightness-75 focus:brightness-75">
 				<i className="text-2xl self-start">
 					{React.createElement(props.icon, null)}
 				</i>
@@ -38,22 +38,27 @@ export const Sidebar = () => {
 		<Dialog open={isOpen} onClose={() => setIsOpen(false)} className="z-50">
 			<div className="fixed inset-0 bg-black-800/60" aria-hidden="true" />
 
-			<Dialog.Panel className="w-full h-1/2 flex-center--col absolute right-0 top-0 z-45 bg-black-700">
-				<Dialog.Title className="hidden">Barra de navegação</Dialog.Title>
-				<button
-					onClick={() => setIsOpen(false)}
-					className="absolute top-8 right-8 text-2xl text-pink-50"
-				>
-					<BsXLg />
-				</button>
-				<nav className="w-full p-4">
-					<ul>
-						{links.map((link) => (
-							<NavLink {...link} key={link.id} />
-						))}
-					</ul>
-				</nav>
-			</Dialog.Panel>
+			<div className="inset-0 absolute right-0 top-0 z-45 flex flex-col items-end">
+				<Dialog.Panel className="w-full md:max-w-md h-1/2 flex-center--col bg-black-700 relative">
+					<Dialog.Title className="hidden">Barra de navegação</Dialog.Title>
+					<button
+						onClick={() => setIsOpen(false)}
+						className="absolute top-8 right-8 text-2xl text-pink-50"
+					>
+						<BsXLg />
+					</button>
+					<nav className="w-full p-4">
+						<ul>
+							{links.map((link) => (
+								<NavLink {...link} key={link.id} />
+							))}
+						</ul>
+					</nav>
+					<small className="absolute bottom-9 font-medium">
+						&copy; FunMath! - {new Date().getFullYear()}
+					</small>
+				</Dialog.Panel>
+			</div>
 		</Dialog>
 	);
 };
