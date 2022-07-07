d9f2bccce7738024c6cb3d2ddd7f14c684eaba97
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-07 15:55:14 -0300

    feat: cria contexto para operador selecionado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
 
 import { Seo } from "../components/Infra";
+
+import { client } from "../lib/client";
+import { SelectedOperatorContextProvider } from "../contexts/SelectedOperatorContext";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<>
-			<Seo />
-			<Component {...pageProps} />
-		</>
+		<ApolloProvider client={client}>
+			<SelectedOperatorContextProvider>
+				<Seo />
+				<Component {...pageProps} />
+			</SelectedOperatorContextProvider>
+		</ApolloProvider>
 	);
 }
 
