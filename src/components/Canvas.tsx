efd331ae09bf9f34ab6afb717771d45cf7d27310
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-23 18:50:11 -0300

    feat: adiciona tags de acessibilidade e separa componentes

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,67 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 
-import { useBasicCanvas, useCanvasSuperset } from "../hooks/Canvas/";
+import { useBasicCanvas } from "../hooks/Canvas/";
 
-import { Range } from "./Range";
-import { ColorPicker } from "./ColorPicker";
-import { IconButton } from "./Buttons/IconButton";
-import { MainButton } from "./Buttons";
+import { Tools } from "./Tools";
 
 import { CanvasEvent } from "../types";
-import { CanvasRef, ContextRef } from "../types/hooks";
-
 import { createMouseEvent } from "../utils/createMouseEvent";
-import { tools } from "../constants";
-
-interface ToolsProps {
-	canvasRef: CanvasRef;
-	contextRef: ContextRef;
-}
-
-const Tools = (props: ToolsProps) => {
-	const { currentTool, changeCurrentTool, changeColor, changeWidth, clear } =
-		useCanvasSuperset(props.canvasRef, props.contextRef);
-
-	return (
-		<section className="flex items-center justify-between p-2 bg-canvas-tools rounded-br-md rounded-bl-md">
-			<div className="flex-center--row gap-3 md:gap-6">
-				<ColorPicker color={currentTool.color} changeColor={changeColor} />
-				{tools.list.map((tool) => (
-					<IconButton
-						type="button"
-						title={tool.name}
-						onClick={() => changeCurrentTool(tool.id)}
-						className={`${
-							tool.id === currentTool.type &&
-							"bg-main/40 rounded-md text-white/40 transition-all"
-						} p-2`}
-						icon={{
-							element: tool.icon,
-							label: `Icone de ${tool.name}`,
-						}}
-					/>
-				))}
-
-				<Range
-					id="lineWidth"
-					name="lineWidth"
-					label="Mude a largura dos traços"
-					min="1"
-					max="30"
-					value={currentTool.width.toString()}
-					changeValue={changeWidth}
-				/>
-			</div>
-			<MainButton
-				type="button"
-				onClick={clear}
-				className="rounded-md p-[10px] md:p-3 text-2xs md:text-xs uppercase tracking-wider"
-			>
-				Limpar
-			</MainButton>
-		</section>
-	);
-};
 
 const CLASS_SCROLL_BLOCK = "scroll--block";
 
@@ -116,6 +60,7 @@ export const Canvas = () => {
 	return (
 		<>
 			<canvas
+				id="to-draw"
 				ref={canvasRef}
 				onMouseDown={startDrawing}
 				onTouchStart={({ touches }) =>
