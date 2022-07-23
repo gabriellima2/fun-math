396954afe83436edded8f8a31e622e93db40a93b
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-23 18:48:55 -0300

    feat: adiciona responsividade com novos estilos

--- src/components/InsertAnswer.tsx
+++ src/components/InsertAnswer.tsx
@@ -6,14 +6,15 @@ interface InsertAnswerProps {
 export const InsertAnswer = (props: InsertAnswerProps) => {
 	return (
 		<>
-			<label htmlFor="" className="font-util text-sm md:text-base">
+			<label htmlFor="" className="font-util text-base md:text-lg">
 				<span className="font-semibold mr-1">R:</span>O resultado é{" "}
 			</label>
 			<input
 				type="number"
 				value={props.value}
 				onChange={(e) => props.changeValue(e.target.value)}
-				className="w-16 h-7 p-1 px-2 ml-2 font-util text-sm font-medium tracking-wide rounded-lg bg-white/20 transition-all outline-custom--focus focus:bg-transparent"
+				placeholder="Digite aqui..."
+				className="h-10 p-1 px-2 ml-2 font-util text-sm md:text-base font-medium tracking-wide rounded-lg bg-transparent border-2 border-white/30 transition-all outline-custom--focus focus:border-transparent"
 			/>
 		</>
 	);
