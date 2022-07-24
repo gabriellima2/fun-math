490a140ce095ab29f8378c9f957074351d0aabf8
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-23 22:48:40 -0300

    feat: adiciona acessibilidade e melhor responsividade ao InsertAnswer

--- src/components/InsertAnswer.tsx
+++ src/components/InsertAnswer.tsx
@@ -6,15 +6,17 @@ interface InsertAnswerProps {
 export const InsertAnswer = (props: InsertAnswerProps) => {
 	return (
 		<>
-			<label htmlFor="" className="font-util text-base md:text-lg">
+			<label htmlFor="insert-answer" className="font-util text-base md:text-lg">
 				<span className="font-semibold mr-1">R:</span>O resultado é{" "}
 			</label>
 			<input
 				type="number"
+				name="answer"
+				id="insert-answer"
 				value={props.value}
 				onChange={(e) => props.changeValue(e.target.value)}
 				placeholder="Digite aqui..."
-				className="h-10 p-1 px-2 ml-2 font-util text-sm md:text-base font-medium tracking-wide rounded-lg bg-transparent border-2 border-white/30 transition-all outline-custom--focus focus:border-transparent"
+				className="w-32 md:w-auto h-10 p-1 px-2 font-util text-sm md:text-base font-medium tracking-wide rounded-lg bg-transparent border-2 border-white/30 transition-all outline-custom--focus focus:border-transparent"
 			/>
 		</>
 	);
