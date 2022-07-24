1fca764641aeaf89a1bb66db29399a0eed6d7597
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-07-23 22:47:22 -0300

    feat: melhor ux e lógica para Status

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,26 +1,31 @@
+import { BsFillCheckSquareFill, BsFillXSquareFill } from "react-icons/bs";
+
 interface StatusProps {
-	type: "success" | "error" | "default";
+	type: "success" | "error";
 }
 
+const Success = () => (
+	<i
+		aria-labelledby="status"
+		aria-label="Resposta Certa"
+		className="text-green-600"
+	>
+		<BsFillCheckSquareFill />
+	</i>
+);
+
+const Error = () => (
+	<i
+		aria-labelledby="status"
+		aria-label="Resposta Errada"
+		className="text-red-600"
+	>
+		<BsFillXSquareFill />
+	</i>
+);
+
 export const Status = (props: StatusProps) => (
-	<div className="container w-full flex-center--row gap-2 bg-canvas-tools">
-		{props.type == "default" ? (
-			<>
-				<span className="block w-full p-1 rounded-lg bg-black-100"></span>
-				<p className="text-center text-white/40 font-util font-semibold">
-					Sem Dados!
-				</p>
-			</>
-		) : props.type == "error" ? (
-			<>
-				<span className="block w-full p-1 rounded-lg bg-red-400"></span>
-				<p className="text-red-300 font-util font-semibold"> Errado!</p>
-			</>
-		) : (
-			<>
-				<span className="block w-full p-1 rounded-lg bg-green-400"></span>
-				<p className="text-green-300 font-util font-semibold"> Certo!</p>
-			</>
-		)}
+	<div id="status" role="status" className="text-3xl">
+		{props.type == "success" ? <Success /> : <Error />}
 	</div>
 );
