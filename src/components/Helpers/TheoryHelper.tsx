fb688d3b02ee2f63f42fb527371180da04d8a934
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-02 23:46:47 -0300

    feat: adiciona estrutura para ajuda sobre o operador

--- src/components/Helpers/TheoryHelper.tsx
+++ src/components/Helpers/TheoryHelper.tsx
@@ -1,5 +1,35 @@
-const ButtonText = () => <>Teoria</>;
+import Image from "next/image";
+import { useState } from "react";
 
-const Content = () => <h1>Teoria</h1>;
+import { Operators } from "../Operators";
+import { Radio } from "../Radio";
+
+import { operators } from "../../constants";
+
+const ButtonText = () => <>Sobre o operador</>;
+
+const Content = () => {
+	const [currentOperator, setCurrentOperator] = useState("");
+
+	const changeCurrentOperator = (operator: string) =>
+		setCurrentOperator(operator);
+
+	return (
+		<>
+			<Operators.Container
+				handleChange={changeCurrentOperator}
+				currentActiveOption={currentOperator}
+				className="flex gap-2 overflow-x-scroll pb-4"
+			>
+				<Operators.List
+					showOperatorName={false}
+					className="w-fit"
+					sizeImage="w-6 h-6"
+				/>
+			</Operators.Container>
+			{currentOperator && <p>Texto do operador de {currentOperator}</p>}
+		</>
+	);
+};
 
 export const TheoryHelper = { ButtonText, Content };
