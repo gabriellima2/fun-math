8c5f79d83495738e9a11cfeab150f19bf63235e0
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-03 22:46:21 -0300

    fix

--- src/components/Infra/index.ts
+++ src/components/Infra/index.ts
@@ -2,5 +2,7 @@ import { Copyright } from "./Copyright";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Seo } from "./Seo";
+import { Loading } from "./Loading";
+import { Error } from "./Error";
 
-export { Copyright, Header, Footer, Seo };
+export { Copyright, Header, Footer, Seo, Loading, Error };
