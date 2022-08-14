dfa361353ffcc93fbb577fa05844fec35086b33d
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-14 17:16:25 -0300

    feat: adiciona elementos para melhorar UX nos erros

--- src/components/Infra/Error.tsx
+++ src/components/Infra/Error.tsx
@@ -1,15 +1,34 @@
+import { TextLink } from "../Links";
+
+import { Logo } from "./Logo";
+
+import { ClassName } from "../../types";
+
 interface ErrorProps {
 	message: string;
+	className?: ClassName;
+}
+
+interface FullScreenProps extends ErrorProps {
+	withLogo?: boolean;
 }
 
 const Text = (props: ErrorProps) => (
-	<p className="text-sm md:text-base">{props.message}</p>
+	<p className={`${props.className} `}>{props.message}</p>
 );
 
-const FullScreen = (props: ErrorProps) => (
-	<div className="gradient-container">
-		<Text message={props.message} />
+const FullScreen = ({ withLogo, ...props }: FullScreenProps) => (
+	<div className="gradient-background gap-4 px-4">
+		{withLogo && <Logo />}
+		<Text {...props} />
+		<TextLink href="/">Voltar para o início</TextLink>
 	</div>
 );
+
+const defaultProps: Pick<ErrorProps, "className"> = {
+	className: "text-sm md:text-base",
+};
+
+Text.defaultProps = defaultProps;
 
 export const Error = { Text, FullScreen };
