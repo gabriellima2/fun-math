bf35b78d72da317311c66667ff60fe39efa957ad
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-13 23:12:03 -0300

    feat: rota para área de rascunho

---
+++ src/pages/draw.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+
+import { Canvas } from "../components/Draw";
+import { Customized } from "../layouts/Customized";
+
+const Draw: NextPage = () => {
+	return (
+		<Customized>
+			<main className="w-full h-full flex-center--col mt-6 sm:mt-10">
+				<div className="w-[95vw] xl:w-[70vw] max-w-fit md:max-h-[1/2] overflow-x-hidden">
+					<Canvas />
+				</div>
+			</main>
+		</Customized>
+	);
+};
+
+export default Draw;
