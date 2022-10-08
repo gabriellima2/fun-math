57dc0a9a2275606efccf50c1ffa71f67ca97605a
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-07 22:59:42 -0300

    test: adiciona testes para input text

---
+++ src/__tests__/components/Inputs/Text.spec.tsx
@@ -0,0 +1,46 @@
+import "@testing-library/jest-dom";
+
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+
+import { Input } from "../../../components/Inputs";
+
+const PLACEHOLDER_TEXT = "Testing";
+const TYPED_VALUE = "Input Test";
+
+function getTextInput() {
+	return screen.getByPlaceholderText(PLACEHOLDER_TEXT);
+}
+
+describe("Text Input Component", () => {
+	const setStateMock = jest.fn();
+
+	jest.mock("react", () => ({
+		useState: (initial: unknown) => [initial, setStateMock],
+	}));
+
+	beforeEach(() => {
+		render(
+			<Input.Text
+				id="text"
+				name="text"
+				value=""
+				onChange={setStateMock}
+				placeholder={PLACEHOLDER_TEXT}
+			>
+				Test
+			</Input.Text>
+		);
+	});
+
+	it("should render the component", () => {
+		expect(getTextInput()).toBeInTheDocument();
+	});
+
+	it("should handle changes in input", async () => {
+		await userEvent.type(getTextInput(), TYPED_VALUE);
+
+		expect(setStateMock).toHaveBeenCalled();
+	});
+});
