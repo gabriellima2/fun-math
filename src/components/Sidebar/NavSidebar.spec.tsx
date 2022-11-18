038259df761b09b3ca0f0567fa9e55da0b5a92fe
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-11-18 20:02:27 -0300

    test: aumenta coverage dos teste dos elementos da sidebar

--- src/components/Sidebar/NavSidebar.spec.tsx
+++ src/components/Sidebar/NavSidebar.spec.tsx
@@ -6,7 +6,8 @@ import { render, screen } from "@testing-library/react";
 import { NavSidebar } from "./NavSidebar";
 import type { ILink } from "@interfaces/ILink";
 
-const ANCHOR_TEXT = "Go to test";
+const FIRST_TEXT = "First";
+const SECOND_TEXT = "Second";
 
 jest.mock("next/router", () => ({
 	useRouter() {
@@ -19,13 +20,10 @@ jest.mock("next/router", () => ({
 	},
 }));
 
-function getAnchor() {
-	return screen.getByText(ANCHOR_TEXT);
-}
-
 describe("Nav Sidebar Component", () => {
 	const linksMock: ILink[] = [
-		{ id: 1, name: ANCHOR_TEXT, href: "/", icon: BsApp },
+		{ id: 1, name: FIRST_TEXT, href: "/", icon: BsApp },
+		{ id: 2, name: SECOND_TEXT, href: "/fazer-exercicios", icon: BsApp },
 	];
 	const closeSidebarMock = jest.fn();
 
@@ -34,11 +32,22 @@ describe("Nav Sidebar Component", () => {
 	});
 
 	it("should render", () => {
-		expect(getAnchor()).toBeInTheDocument();
+		expect(screen.getByText(FIRST_TEXT)).toBeInTheDocument();
 	});
 
 	it("should be prevent focus if on current route", async () => {
-		expect(getAnchor()).toHaveAttribute("aria-disabled", "true");
-		expect(getAnchor()).toHaveAttribute("tabindex", "-1");
+		expect(screen.getByText(FIRST_TEXT)).toHaveAttribute(
+			"aria-disabled",
+			"true"
+		);
+		expect(screen.getByText(FIRST_TEXT)).toHaveAttribute("tabindex", "-1");
+	});
+
+	it("should be default behavior if not on current route", async () => {
+		expect(screen.getByText(SECOND_TEXT)).toHaveAttribute(
+			"aria-disabled",
+			"false"
+		);
+		expect(screen.getByText(SECOND_TEXT)).toHaveAttribute("tabindex", "0");
 	});
 });
